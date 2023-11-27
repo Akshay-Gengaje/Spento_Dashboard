812e2a2e4d60bc8b44201a3e2b61c38a03454345
Akshay-Gengaje <akshaysgengaje@gmail.com>
2023-11-27 12:31:48 +0530

    modified:   src/App.jsx	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardLayout from "./layout/Dashboard/DashboardLayout";
 import Attendence from "./pages/Admin/Attendence";
-import Profile from "./pages/admin/profile/Profile";
+import Profile from "./pages/admin/profile/Profile.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 // import DashboardContainer from "./layout/Container/DashboardContainer";
